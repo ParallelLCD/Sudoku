3a8606ab3ac2969ac25fd1432d233b407d572362
Justin <35553410+ParallelLCD@users.noreply.github.com>
2020-06-13 20:29:24 -0700

    Added "completed" code
    Algorithm to generate a Sudoku board needs to be rewritten.

---
+++ CheckBoard.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace sudoku
+{
+    class CheckBoard
+    {
+
+        public int?[,] board;
+        public int?[,] checkboard;
+        public int sq;
+        public int num;
+        public int row;
+        public int col;
+
+
+
+        public void PrintArray (int?[,] matrix)
+        {
+            Console.Write("\t");
+            for (int i = 0; i < sq; i++)
+            {
+                if (i == 2 || i == 5)
+                    Console.Write($" {i + 1}\t\t");
+                else
+                    Console.Write($" {i + 1}\t");
+            }
+            Console.Write("\n\n");
+            for (int j = 0; j < sq; j++)
+            {
+                Console.Write(j + 1 + "\t");
+                for (int k = 0; k < sq; k++)
+                {
+                    if (checkboard[j, k] == null)
+                        Console.Write("|" + matrix[j, k]);
+                    else
+                        Console.Write("|" + matrix[j, k] + "*");
+
+                    if (k == 2 || k == 5)
+                        Console.Write("|\t|\t");
+                    else
+                        Console.Write("|\t");
+                }
+                Console.Write("\n\n");
+                if (j == 2 || j == 5)
+                    Console.Write("\t _\t _\t _\t_\t _\t _\t _\t_\t _\t _\t _\n\n");
+            }
+        }
+
+
+
+        public bool CheckIfMoveIsValid (int row, int col)
+        {
+            if (checkboard[row, col] == null)
+            {
+                Console.WriteLine("You can only place numbers on empty blocks or replace numbers you set.");
+                return false;
+            }
+            return true;
+        }
+
+
+
+        public bool CheckIfFull (int?[,] matrix)
+        {
+            int count = 0;
+            for (int i = 0; i < sq; i++)
+            {
+                for (int j = 0; j < sq; j++)
+                {
+                    if (matrix[i, j] != null) { count++; }
+                }
+            }
+            if (count == sq*sq) { return true; }
+            else { return false; }
+        }
+
+
+
+        public bool RuleCheck(int?[,] matrix, int row, int col, int num)
+        {
+            matrix[row, col] = null;
+            for (int i = 0; i < sq; i++)
+            {
+                if (matrix[row, i] == num) { return false; }
+            }
+
+            for (int j = 0; j < sq; j++)
+            {
+                if (matrix[j, col] == num) { return false; }
+            }
+
+            for (int k = 0; k < sq; k+=3)
+            {
+                if (row >= k && row < 3 + k)
+                {
+                    if (!ColumnCheck(matrix, col, num, k)) { return false; }
+                }
+            }
+            matrix[row, col] = num;
+            return true;
+        }
+
+
+
+        private bool ColumnCheck(int?[,] matrix, int col, int num, int addRow)
+        {
+            for (int i = 0; i < sq; i+=3)
+            {
+                if (col >= i && col < 3 + i)
+                {
+                    if (!SquareCheck(matrix, num, addRow, i)) { return false; }
+                }
+            }
+            return true;
+        }
+
+
+
+        private bool SquareCheck(int?[,] matrix, int num, int addRow, int addCol)
+        {
+           for (int i = addRow; i < 3 + addRow; i++)
+           {
+                for (int j = addCol; j < 3 + addCol; j++)
+                {
+                    if (matrix[i, j] == num) { return false; }
+                }
+           }
+           return true;
+        }
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,120 +1,211 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Security.Cryptography;
 
 namespace sudoku
-{
-    class CheckBoard
-    {
-        public static int?[,] board;
-        public static int sq;
-        public static void PrintArray(int?[,] matrix, int size)
-        {
-            Console.Write("\t");
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write(i + 1 + "\t");
-            }
-            Console.Write("\n\n");
-            for (int j = 0; j < size; j++)
-            {
-                Console.Write(j + 1 + " \t");
-                for (int k = 0; k < size; k++)
-                {
-                    Console.Write("|" + matrix[j, k] + "|\t");
-                }
-                Console.Write("\n\n");
-            }
-        }
-        public static bool RuleCheck(int?[,] matrix, int row, int col, int num, int size)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                if (matrix[row, i] == num) { return false; }
-            }
-            for (int j = 0; j < size; j++)
-            {
-                if (matrix[j, col] == num) { return false; }
-            }
-
-            if (row < 3)
-            {
-                if (!ColumnCheck(matrix, col, num, 0)) { return false; }
-            }
-            else if (row >= 3 && row < 6)
-            {
-                if (!ColumnCheck(matrix, col, num, 3)) { return false; }
-            }
-            else
-            {
-                if (!ColumnCheck(matrix, col, num, 6)) { return false; }
-            }
-            return true;
-        }
-        private static bool ColumnCheck(int?[,] matrix, int col, int num, int addRow)
-        {
-            if (col < 3)
-            {
-                if (!SquareCheck(matrix, num, addRow, 0)) { return false; }
-            }
-            else if (col >= 3 && col < 6)
-            {
-                if (!SquareCheck(matrix, num, addRow, 3)) { return false; }
-            }
-            else
-            {
-                if (!SquareCheck(matrix, num, addRow, 6)) { return false; }
-            }
-            return true;
-        }
-        private static bool SquareCheck(int?[,] matrix, int num, int addRow, int addCol)
-        {
-            for (int i = 0 + addRow; i < 3 + addRow; i++)
-            {
-                for (int j = 0 + addCol; j < 3 + addCol; j++)
-                {
-                    if (matrix[i,j] == num) { return false; }
-                }
-            }
-            return true;
-        }
-    }
-    class GenBoard : CheckBoard
-    {
-
-        private static int counter;
-        private static bool pass;
-        static GenBoard()
-        {
-            sq = 9;
-            counter = 10; //amount of numbers allocated to the board
-            board = new int?[sq, sq];
-            Random rand = new Random();
-            for (int i = 0; i < counter; i++)
-            {
-                pass = false;
-                int num = rand.Next(1, 10);
-                while(!pass)
-                {
-                    int row = rand.Next(0, sq);
-                    int col = rand.Next(0, sq);
-                    bool check = RuleCheck(board, row, col, num, sq);
-                    if (board[row, col] == null && check == true)
-                    {
-                        board[row, col] = num;
-                        pass = true;
-                    }
-                }
-            }
-        }
-    }
+{     
     class Program
     {
+
         static void Main(string[] args)
         {
-            new GenBoard();
-            GenBoard.PrintArray(GenBoard.board, GenBoard.sq);
-            int?[,] sudokuBoard = GenBoard.board;
+            GenBoard gen1 = new GenBoard();
+            GenBoard gen2 = new GenBoard();
+            int?[,] currentBoard = gen1.board;
+            int?[,] nextBoard = gen2.board;
+
+            bool check = false;
+            bool checkOptions = false;
+            bool checkPlaying = false;
+            bool checkValid = false;
+            bool checkNewGame = false;
+            bool confirmExit = false;
+            bool checkGen = false;
+
+            string input;
+            int counter;
+
+            Console.WriteLine("Welcome to Sudoku.\nPress any key to continue");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            do
+            {
+
+                do
+                {
+                    Console.WriteLine("Select a diffculty:\n[1]Easy [2]Medium [3]Hard");
+                    check = false;
+                    input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "1": gen1.diffculty(50); checkOptions = true; break;
+                        case "2": gen1.diffculty(60); checkOptions = true; break;
+                        case "3": gen1.diffculty(70); checkOptions = true; break;
+                        default: Console.WriteLine("Enter a valid input"); checkOptions = false; break;
+                    }
+                } while (!checkOptions);
+
+
+
+                do
+                {
+                    checkOptions = false;
+                    checkPlaying = false;
+                    counter = 0;
+                    
+
+                    do
+                    {
+                        gen1.PrintArray(currentBoard);
+                        Console.WriteLine("[1]Insert Number [2]Generate New Board [3]Exit");
+                        input = Console.ReadLine();
+                        switch (input)
+                        {
+                            case "1": checkOptions = true; checkPlaying = true; break;
+                            case "2": checkOptions = true; 
+                                Console.WriteLine("Are You Sure?(y/n)");
+                                if(nodInput()) { checkGen = true; checkOptions = true; }
+                                else { checkGen = false; checkOptions = true; }
+                                break;
+                            case "3": checkOptions = true; confirmExit = true;  break;
+                            default: Console.WriteLine("Enter a valid input"); checkOptions = false; break;
+                        }
+                    } while (!checkOptions);
+
+
+                    if (checkPlaying)
+                    {
+
+                        while (!checkValid)
+                        {
+                            Console.WriteLine("Enter a row:");
+                            input = ReadInput();
+                            gen1.row = Convert.ToInt32(input);
+
+                            Console.WriteLine("Enter a column:");
+                            input = ReadInput();
+                            gen1.col = Convert.ToInt32(input);
+
+                            if (gen1.CheckIfMoveIsValid(gen1.row - 1, gen1.col - 1)) { checkValid = true; }
+                        }
+                        checkValid = false;
+
+
+
+                        Console.WriteLine("Enter a number:");
+                        input = ReadInput();
+                        gen1.num = Convert.ToInt32(input);
+
+                        currentBoard[gen1.row - 1, gen1.col - 1] = gen1.num;
+
+
+
+                        if (gen1.CheckIfFull(currentBoard))
+                        {
+                            for (int i = 0; i < gen1.sq; i++)
+                            {
+                                for (int j = 0; j < gen1.sq; j++)
+                                {
+                                    int temp = currentBoard[i, j].Value;
+                                    if (gen1.RuleCheck(currentBoard, i, j, currentBoard[i, j].Value)) { counter++; }
+                                    else { currentBoard[i, j] = temp; }
+                                }
+                            }
+
+                            if (counter == gen1.sq * gen1.sq)
+                            {
+                                Console.WriteLine("YOU ARE WINNER!");
+                                gen1.PrintArray(currentBoard);
+                                check = true;
+                                Console.WriteLine("Play Again?(y/n)");
+                                if (!nodInput()) { checkNewGame = true; }
+                                else { checkGen = true; }
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("Board isn't correct!\nPlease check again");
+                            }
+                        }
+
+
+                    }
+
+                    else if (confirmExit)
+                    {
+                        Console.WriteLine("Are You Sure?(y/n)");
+                        if (nodInput()) { check = true; checkNewGame = true; }
+                    }
+
+                    if (checkGen)
+                    {
+                        gen1 = gen2;
+                        currentBoard = nextBoard;
+                        gen2 = new GenBoard();
+                        nextBoard = gen2.board;
+                        check = true;
+                        checkGen = false;
+                    }
+
+
+                } while (!check);
+
+
+            } while (!checkNewGame);
+        }
+
+
+
+        static private bool nodInput()
+        {
+            bool check;
+            string input;
+
+            do
+            {
+                input = Console.ReadLine();
+                if (input == "y" || input == "Y") { check = true; return true; }
+                else if (input == "n" || input == "N") { check = true; return false; }
+                else { Console.WriteLine("Enter a valid input"); check = false; }
+
+            } while (!check);
+            return false;
+        }
+
+
+
+        static private string ReadInput()
+        {
+            bool move = false;
+            string input;
+
+            do
+            {
+                input = Console.ReadLine();
+                move = CheckInput(input);
+            } while (!move);
+
+            return input;
+        }
+        
+
+
+        static private bool CheckInput(string value)
+        {
+            int input;
+
+            if (!int.TryParse(value, out input))
+            {
+                Console.WriteLine("Input is not a number! (1-9):");
+                return false;
+            }
+            if (input > 9) 
+            {
+                Console.WriteLine("Enter a valid number! (1-9):");
+                return false;
+            }
+
+            return true;
         }
     }
 }
+
